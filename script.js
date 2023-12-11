03a24f48a41b67cf80fcdb76aafe355dca06a47c
NicollasDavi <132391248+NicollasDavi@users.noreply.github.com>
2023-12-11 17:52:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
+    let cardCarussel = document.querySelectorAll(".cardCarussel");
+    let currentIndex = 0;
+    let intervalId;
+
+    function toggleCards(index) {
+        cardCarussel.forEach(function (card, i) {
+            if (i === index) {
+                card.classList.add('active');
+            } else {
+                card.classList.remove('active');
+            }
+        });
+    }
+
+    function startAutoPlay() {
+        intervalId = setInterval(function () {
+            toggleCards(currentIndex);
+            currentIndex = (currentIndex + 1) % cardCarussel.length;
+        }, 3000);
+    }
+
+    function stopAutoPlay() {
+        clearInterval(intervalId);
+    }
+
+    cardCarussel.forEach(function (card, index) {
+        card.addEventListener('mouseenter', function () {
+            toggleCards(index);
+            stopAutoPlay();
+        });
+
+        card.addEventListener('mouseleave', function () {
+            startAutoPlay();
+        });
+    });
+
+    startAutoPlay();
+
     let navigation = document.querySelector(".navigation");
 
     document.querySelector('.menuToggle').addEventListener("click", function () {
